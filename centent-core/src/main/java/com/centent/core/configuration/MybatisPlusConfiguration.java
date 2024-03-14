6b07383c8a731f609592f4c54bdc612720666570
kales <dethan0202@163.com>
2024-03-14 10:58:08 +0800

    feat(ocr): ocr and hua nong test3

--- centent-core/src/main/java/com/centent/core/configuration/MybatisPlusConfiguration.java
+++ centent-core/src/main/java/com/centent/core/configuration/MybatisPlusConfiguration.java
@@ -22,7 +22,7 @@ public class MybatisPlusConfiguration {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        // TODO...动态设置DbType
+        // TODO... 动态设置DbType
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL)); // 如果配置多个插件,切记分页最后添加
         // interceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 如果有多数据源可以不配具体类型 否则都建议配上具体的DbType
         return interceptor;
