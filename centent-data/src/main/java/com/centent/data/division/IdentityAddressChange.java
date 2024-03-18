6bf78a7313017c75bf449b79d6a853443480c60c
kales <dethan0202@163.com>
2024-03-18 22:21:42 +0800

    'fix(身份证)身份证地址计算错误'

--- centent-data/src/main/java/com/centent/data/division/IdentityAddressChange.java
+++ centent-data/src/main/java/com/centent/data/division/IdentityAddressChange.java
@@ -1,2 +1,35 @@
-package com.centent.data.division;public class IdentityAddressChange {
+package com.centent.data.division;
+
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.centent.core.define.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 中华人民共和国行政区划表 - 全量变化数据
+ *
+ * @since 0.0.1
+ */
+@Data
+@TableName("identity_address_change")
+@EqualsAndHashCode(callSuper = true)
+public class IdentityAddressChange extends BaseEntity<IdentityAddressChange> {
+
+    /**
+     * 原行政区划代码
+     *
+     * @since 0.0.1
+     */
+    private Integer code;
+
+    /**
+     * 新行政区划代码
+     */
+    private Integer newCode;
+
+    /**
+     * 变化年份
+     */
+    private Integer time;
 }
--- centent-data/src/main/resources/sql/ddl.sql
+++ centent-data/src/main/resources/sql/ddl.sql
@@ -19,3 +19,23 @@ COMMENT ON COLUMN identity_address.ends IS '废止年份';
 COMMENT ON COLUMN identity_address.remark IS '备注';
 COMMENT ON COLUMN identity_address.create_time IS '创建时间';
 COMMENT ON COLUMN identity_address.update_time IS '更新时间';
+
+CREATE TABLE IF NOT EXISTS identity_address_change
+(
+    id          VARCHAR(32) PRIMARY KEY,
+    code        INT       NOT NULL,
+    new_code    INT       NOT NULL,
+    time        INT,
+    remark      VARCHAR(4000),
+    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+COMMENT ON TABLE identity_address_change IS '中华人民共和国行政区划-变化数据表';
+COMMENT ON COLUMN identity_address_change.id IS '主键';
+COMMENT ON COLUMN identity_address_change.code IS '原行政区划代码';
+COMMENT ON COLUMN identity_address_change.new_code IS '新行政区划代码';
+COMMENT ON COLUMN identity_address_change.time IS '变化年份';
+COMMENT ON COLUMN identity_address_change.remark IS '备注';
+COMMENT ON COLUMN identity_address_change.create_time IS '创建时间';
+COMMENT ON COLUMN identity_address_change.update_time IS '更新时间';
