cad2f89b674ccf076101e2bf6731596fd646dbcf
ethan <dethan0202@gmail.com>
2024-03-19 01:24:56 +0800

    feat(身份证): 身份证数据

---
+++ centent-data/src/main/java/com/centent/data/division/mapper/IdentityAddressChangeMapper.java
@@ -0,0 +1,9 @@
+package com.centent.data.division.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.centent.data.division.IdentityAddressChange;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IdentityAddressChangeMapper extends BaseMapper<IdentityAddressChange> {
+}
