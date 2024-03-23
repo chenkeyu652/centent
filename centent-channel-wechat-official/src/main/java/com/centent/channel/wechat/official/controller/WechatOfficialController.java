135587f19d08aa57c97394185dc34c77a2f02faf
ethan <dethan0202@gmail.com>
2024-03-23 21:51:01 +0800

    feat(身份证): 身份证下拉

--- centent-channel-wechat-official/src/main/java/com/centent/channel/wechat/official/controller/WechatOfficialController.java
+++ centent-channel-wechat-official/src/main/java/com/centent/channel/wechat/official/controller/WechatOfficialController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/wechat/official")
+@RequestMapping("centent/wechat/official")
 public class WechatOfficialController {
 
     /**
