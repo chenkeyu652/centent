c4eb73f80aff9ffe6d9a6c4be4eb6521e49df704
ethan <dethan0202@gmail.com>
2024-03-24 18:01:19 +0800

    fix(order): retrofit忽略不存在的属性

--- centent-core/src/main/java/com/centent/core/retrofit/IRetrofitDefine.java
+++ centent-core/src/main/java/com/centent/core/retrofit/IRetrofitDefine.java
@@ -1,5 +1,7 @@
 package com.centent.core.retrofit;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -10,6 +12,10 @@ public interface IRetrofitDefine {
     String getBaseUrl();
 
     default Retrofit buildRetrofit() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 反序列化时忽略对象中不存在的json字段
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
         OkHttpClient.Builder client = new OkHttpClient.Builder()
                 // 自定义日志打印
                 .addInterceptor(new CommonInterceptor());
@@ -18,7 +24,7 @@ public interface IRetrofitDefine {
                 .baseUrl(this.getBaseUrl())
                 .client(client.build())
                 .addConverterFactory(ScalarsConverterFactory.create())
-                .addConverterFactory(JacksonConverterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                 .build();
     }
 }
